b04deb7169675ce59e0c1a97362aa5d3c47a4b9f
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-02 15:15:06 -0500

    Add comments

--- Core/Src/ILI9341_STM32_Driver.c
+++ Core/Src/ILI9341_STM32_Driver.c
@@ -15,6 +15,7 @@ void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
 // GPDMA1_Channel2
 static void ILI9341_SPI_Tx(uint8_t data)
 {
+	// Check GPDMA busy Flag
 	while((GPDMA1_Channel2->CSR & 0x100));//(!__HAL_SPI_GET_FLAG(HSPI_INSTANCE, SPI_FLAG_TXP)); // Edited From SPI_FLAG_TXE
 	HAL_SPI_Transmit_DMA(HSPI_INSTANCE, &data, 1);
 	//HAL_SPI_Transmit(HSPI_INSTANCE, &data, 1, 10);
@@ -22,6 +23,7 @@ static void ILI9341_SPI_Tx(uint8_t data)
 
 static void ILI9341_SPI_TxBuffer(uint8_t *buffer, uint16_t len)
 {
+	// Check GPDMA busy Flag
 	while((GPDMA1_Channel2->CSR & 0x100));//(!__HAL_SPI_GET_FLAG(HSPI_INSTANCE, SPI_FLAG_TXP)); // Edited From SPI_FLAG_TXE
 	HAL_SPI_Transmit_DMA(HSPI_INSTANCE, buffer, len);
 	//HAL_SPI_Transmit(HSPI_INSTANCE, buffer, len, 10);
